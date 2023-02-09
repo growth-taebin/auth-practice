02d22eaf761cdc464e594292cf62d6c4d8e3126a
yoontaebin <gusckd000@gmail.com>
2023-02-09 20:35:37 +0900

    Add UserRepository 추가

---
+++ src/main/kotlin/com/example/auth/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.example.auth.repository
+
+import com.example.auth.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long> {
+
+    fun findByEmail(email: String?): User?
+
+}
