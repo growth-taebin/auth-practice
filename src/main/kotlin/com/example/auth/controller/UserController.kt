eaccf5da1cda0beefbef54a782289e50df514b7e
yoontaebin <gusckd000@gmail.com>
2023-02-10 18:23:56 +0900

    Add UserController

--- src/main/kotlin/com/example/auth/controller/UserController.kt
+++ src/main/kotlin/com/example/auth/controller/UserController.kt
@@ -1,4 +1,23 @@
 package com.example.auth.controller
 
-class UserController {
+import com.example.auth.dto.request.SignUpRequestDto
+import com.example.auth.repository.UserRepository
+import com.example.auth.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("auth")
+class UserController(
+        private val userService: UserService
+) {
+
+    @PostMapping("/signup")
+    fun signUp(@RequestBody request: SignUpRequestDto): ResponseEntity<Any> =
+            userService.signUp(request)
+                    .let { ResponseEntity.status(HttpStatus.CREATED).build() }
 }
