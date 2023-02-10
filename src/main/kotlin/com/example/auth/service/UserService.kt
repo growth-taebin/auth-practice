95e03f0dbd7270a23406a5b2e6a8008086584595
yoontaebin <gusckd000@gmail.com>
2023-02-10 16:53:24 +0900

    Add UserService 추가

--- src/main/kotlin/com/example/auth/service/UserService.kt
+++ src/main/kotlin/com/example/auth/service/UserService.kt
@@ -1,4 +1,21 @@
 package com.example.auth.service
 
-class UserService {
+import com.example.auth.dto.request.SignUpRequestDto
+import com.example.auth.entity.User
+import com.example.auth.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+        private val userRepository: UserRepository,
+        private val passwordEncoder: PasswordEncoder
+) {
+    fun signUp(id: Long, signUpRequestDto: SignUpRequestDto) {
+        if (userRepository.existsByEmail(signUpRequestDto.email)) {
+            throw RuntimeException()
+        }
+        userRepository.save(User(id = -1, signUpRequestDto.email, signUpRequestDto.password, signUpRequestDto.name))
+
+    }
 }
