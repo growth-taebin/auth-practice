6e09fc13958d3ba7bfa862244717de12e6f3ef98
yoontaebin <gusckd000@gmail.com>
2023-02-14 18:46:37 +0900

    Update signup service 수정

--- src/main/kotlin/com/example/auth/service/UserAuthService.kt
+++ src/main/kotlin/com/example/auth/service/UserAuthService.kt
@@ -3,24 +3,26 @@ package com.example.auth.service
 import com.example.auth.dto.request.SignInRequestDto
 import com.example.auth.dto.request.SignUpRequestDto
 import com.example.auth.entity.User
+import com.example.auth.enumType.Authority
 import com.example.auth.repository.UserRepository
 import com.example.auth.security.jwt.TokenProvider
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.Collections
 
 @Service
 class UserAuthService(
         private val userRepository: UserRepository,
         private val passwordEncoder: PasswordEncoder,
         private val tokenProvider: TokenProvider
-) {
+        ) {
     @Transactional(rollbackFor = [Exception::class])
     fun signUp(signUpRequestDto: SignUpRequestDto) {
         if (userRepository.existsByEmail(signUpRequestDto.email)) {
             throw RuntimeException()
         }
-        userRepository.save(User(id = -1, signUpRequestDto.email, passwordEncoder.encode(signUpRequestDto.password), signUpRequestDto.name))
+        userRepository.save(User(id = -1, signUpRequestDto.email, passwordEncoder.encode(signUpRequestDto.password), signUpRequestDto.name, Collections.singletonList(Authority.ROLE_USER)))
 
     }
 
