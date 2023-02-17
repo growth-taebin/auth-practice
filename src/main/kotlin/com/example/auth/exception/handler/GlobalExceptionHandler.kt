686b2fbfae9214b8e070125f8ec7e29471df5b87
yoontaebin <gusckd000@gmail.com>
2023-02-17 23:28:37 +0900

    Add GlobalExceptionHandler 추가

--- src/main/kotlin/com/example/auth/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/auth/exception/handler/GlobalExceptionHandler.kt
@@ -1,4 +1,19 @@
 package com.example.auth.exception.handler
 
+import com.example.auth.exception.BasicException
+import com.example.auth.exception.response.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
 class GlobalExceptionHandler {
+
+    @ExceptionHandler(BasicException::class)
+    fun globalExceptionHandler(e: BasicException): ResponseEntity<ErrorResponse> =
+            ResponseEntity(
+                    ErrorResponse(e.errorCode.message, e.errorCode.status),
+                    HttpStatus.valueOf(e.errorCode.status)
+            )
 }
