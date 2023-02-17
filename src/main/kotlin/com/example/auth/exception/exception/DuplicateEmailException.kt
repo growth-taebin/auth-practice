c107af0ffc65517d530703b97df3b659d8c8f1b1
yoontaebin <gusckd000@gmail.com>
2023-02-17 23:25:24 +0900

    Add DuplicateEmailException 추가

--- src/main/kotlin/com/example/auth/exception/exception/DuplicateEmailException.kt
+++ src/main/kotlin/com/example/auth/exception/exception/DuplicateEmailException.kt
@@ -1,4 +1,7 @@
 package com.example.auth.exception.exception
 
-class DuplicateEmailException {
+import com.example.auth.exception.BasicException
+import com.example.auth.exception.enumType.ErrorCode
+
+class DuplicateEmailException: BasicException(ErrorCode.DUPLICATE_EMAIL) {
 }
