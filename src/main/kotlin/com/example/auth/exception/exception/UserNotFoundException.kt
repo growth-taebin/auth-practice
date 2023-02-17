e065c35d7f84e7ee337cf4decdf241a3175e997a
yoontaebin <gusckd000@gmail.com>
2023-02-17 23:24:56 +0900

    Add UserNotFoundException 추가

--- src/main/kotlin/com/example/auth/exception/exception/UserNotFoundException.kt
+++ src/main/kotlin/com/example/auth/exception/exception/UserNotFoundException.kt
@@ -1,4 +1,7 @@
 package com.example.auth.exception.exception
 
-class UserNotFoundException {
+import com.example.auth.exception.BasicException
+import com.example.auth.exception.enumType.ErrorCode
+
+class UserNotFoundException: BasicException(ErrorCode.USER_NOT_FOUND) {
 }
