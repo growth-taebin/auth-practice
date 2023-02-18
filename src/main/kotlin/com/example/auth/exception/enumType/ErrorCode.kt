5867a22a77f1cc752c07b4c82e158e4715fc468f
yoontaebin <gusckd000@gmail.com>
2023-02-19 04:21:42 +0900

    Update REFRESH_TOKEN_EXPIRED 추가

--- src/main/kotlin/com/example/auth/exception/enumType/ErrorCode.kt
+++ src/main/kotlin/com/example/auth/exception/enumType/ErrorCode.kt
@@ -9,4 +9,5 @@ enum class ErrorCode(
     DUPLICATE_EMAIL("중복된 이메일 입니다.", 409),
     PASSWORD_NOT_CORRECT("비밀번호가 일치하지 않습니다.", 400),
     INVALID_TOKEN("유효하지 않은 토큰입니다.", 401),
+    REFRESH_TOKEN_EXPIRED("만료된 refreshToken 입니다.", 401)
 }
