7e920714850096f9e743120d6a1cf92e183787bf
yoontaebin <gusckd000@gmail.com>
2023-02-19 04:21:04 +0900

    Add InvalidTokenException 추가

--- src/main/kotlin/com/example/auth/exception/exception/InvalidTokenException.kt
+++ src/main/kotlin/com/example/auth/exception/exception/InvalidTokenException.kt
@@ -1,4 +1,7 @@
 package com.example.auth.exception.exception
 
-class InvalidTokenException {
+import com.example.auth.exception.BasicException
+import com.example.auth.exception.enumType.ErrorCode
+
+class InvalidTokenException: BasicException(ErrorCode.INVALID_TOKEN) {
 }
