c68aa642e66ca64071b5ea644ccb35356c3b238c
yoontaebin <gusckd000@gmail.com>
2023-02-19 04:22:09 +0900

    Add ExpiredRefreshTokenException 추가

--- src/main/kotlin/com/example/auth/exception/exception/ExpiredRefreshTokenException.kt
+++ src/main/kotlin/com/example/auth/exception/exception/ExpiredRefreshTokenException.kt
@@ -1,4 +1,7 @@
 package com.example.auth.exception.exception
 
-class ExpiredRefreshTokenException {
+import com.example.auth.exception.BasicException
+import com.example.auth.exception.enumType.ErrorCode
+
+class ExpiredRefreshTokenException: BasicException(ErrorCode.REFRESH_TOKEN_EXPIRED) {
 }
