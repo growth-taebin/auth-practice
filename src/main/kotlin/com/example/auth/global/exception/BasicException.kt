abb001621618aa3df5bb61445747d225d966f9da
yoontaebin <gusckd000@gmail.com>
2023-02-20 18:27:29 +0900

    Remove 대괄호 제거

--- src/main/kotlin/com/example/auth/global/exception/BasicException.kt
+++ src/main/kotlin/com/example/auth/global/exception/BasicException.kt
@@ -2,5 +2,4 @@ package com.example.auth.global.exception
 
 import com.example.auth.global.exception.enumType.ErrorCode
 
-open class BasicException(val errorCode: ErrorCode): RuntimeException(errorCode.message) {
-}
+open class BasicException(val errorCode: ErrorCode): RuntimeException(errorCode.message)
